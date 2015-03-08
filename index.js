d931f170816b467367cc738c220c0b248789ab9b
Chi Kei Chan <chikeichan@Chis-MacBook-Pro.local>
2015-03-07 18:02:08 -0800

    Done with room; used string for room namespace

--- index.js
+++ index.js
@@ -14,37 +14,33 @@ io.on('connection',function(socket){
     rooms[roomName].players = {};
     rooms[roomName].weiqi   = {};
 
-    socket.join(nsp);
-    io.to(nsp).emit('room', roomName);
+    socket.join('/'+roomName);
+    io.to('/'+roomName).emit('room', roomName);
   })
 
   socket.on('joinGame',function(roomName){
-    var nsp = io.of('/'+roomName);
-    socket.join(nsp);
-    io.to(nsp).emit('room',roomName);
-    io.to(nsp).emit('players',rooms[roomName].players);
-    io.to(nsp).emit('oppoMove',rooms[roomName].weiqi);
+    socket.join('/'+roomName);
+    io.to('/'+roomName).emit('room',roomName);
+    io.to('/'+roomName).emit('players',rooms[roomName].players);
+    io.to('/'+roomName).emit('oppoMove',rooms[roomName].weiqi);
   })
 
   socket.on('join',function(attr){
-    var nsp = io.of('/'+attr.room);
     rooms[attr.room].players[attr.me] = socket.id;
-    io.to(nsp).emit('players',rooms[attr.room].players);
+    io.to('/'+attr.room).emit('players',rooms[attr.room].players);
   })
 
   socket.on('move',function(attr){
-    var nsp = io.of('/'+attr.room);
     rooms[attr.room].weiqi = attr;
-    io.to(nsp).emit('oppoMove',attr);
+    io.to('/'+attr.room).emit('oppoMove',attr);
   })
 
   socket.on('exit',function(attr){
-    var nsp = io.of('/'+attr.room);
     var players = rooms[attr.room].players;
     for(var key in players){
       if(players[key] === socket.id){
         delete players[key];
-        io.to(nsp).emit('leave',socket.id)
+        io.to('/'+attr.room).emit('leave',socket.id)
         // socket.broadcast.emit('leave',socket.id);
       }
     }
